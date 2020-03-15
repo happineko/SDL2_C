64c828b0f002537e3cc3c2cdda7780efef89b63a
happineko <y.bonin@ludus-academie.com>
2020-03-15 22:37:48 +0100

    Quastion 7

--- SDL2.c
+++ SDL2.c
@@ -1,4 +1,4 @@
-//Question 6
+//Question 7
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,6 +13,28 @@ int main(int argc, char** argv) //argc signifie le nombre d'argument qui sera re
         return -1;
     }
 
+    {
+        /* Création de la fenêtre */
+        SDL_Window* pWindow = NULL; //On crée une variable fenetre de type SDL_Window
+
+        //Pour créer la fentre on utilise la fonction SDL_CreateWindow qui attend en parametre un nom,une position x, une position y, une largeur, une hauteur et un ou plusieurs flags (disponible sur le site de SDL dans ce cas la on veut just afficher la fenêtre)
+        pWindow = SDL_CreateWindow("Fenêtre SDL2",SDL_WINDOWPOS_UNDEFINED,
+                                                                  SDL_WINDOWPOS_UNDEFINED,
+                                                                  640,
+                                                                  480,
+                                                                  SDL_WINDOW_SHOWN);
+
+        if( pWindow ) //si la fenetre a bien été créee
+        {
+            SDL_Delay(3000); //On attend trois secondes, que l'utilisateur voit la fenêtre
+
+            SDL_DestroyWindow(pWindow); //On "détruit" la fenetre
+        }
+        else
+        {
+            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError()); //sinon on retourne un code erreur
+        }
+    }
 
     SDL_Quit(); //On libere le sous systeme une fois qu'on a terminer le programme (libère la mémoire)
 

1de6c06f36594e1a36d464b366cd345668450ed9
happineko <y.bonin@ludus-academie.com>
2020-03-15 22:27:21 +0100

    Quastion 6

--- SDL2.c
+++ SDL2.c
@@ -1,1 +1,20 @@
+//Question 6
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h> //On appelle la librairie SDL2 dans le projet
+
+int main(int argc, char** argv) //argc signifie le nombre d'argument qui sera renseigné argv est la liste des argument qu'il recevra
+{
+    /* Initialisation simple */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0 ) //Dans ce cas on initialise le sous systeme video, si l'initialisation échoue on arrete le programme avec un code erreur
+    {
+        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
+        return -1;
+    }
+
+
+    SDL_Quit(); //On libere le sous systeme une fois qu'on a terminer le programme (libère la mémoire)
+
+    return 0;
+}

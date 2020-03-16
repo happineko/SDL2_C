0a8bf0a75f03a12d5e99044de1b577086a2270d5
happineko <y.bonin@ludus-academie.com>
2020-03-16 17:55:31 +0100

    Question 23

--- SDL2.c
+++ SDL2.c
@@ -1,4 +1,4 @@
-//Question 22
+//Question 23
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -82,7 +82,7 @@ int main(int argc, char** argv) //argc signifie le nombre d'argument qui sera re
         SDL_Window* pWindow = NULL; //On crée une variable fenetre de type SDL_Window
         SDL_Renderer* renderer;//Déclaration du renderer
         SDL_Point cercleV, cercleP; //On déclare un point qu'on pourra utiliser plus tard dans le programme
-        SDL_Rect rectangle, carV, carP; //On declare un rectangle qu'on pourra utiliser plus tard dans le programme
+        SDL_Rect rectangle, carV, carP, rTemp; //On declare un rectangle qu'on pourra utiliser plus tard dans le programme
         SDL_Surface *surface, *temp = NULL; //Déclaration d'une surface
         SDL_Texture *texture; //Declaration de la texture
         int nLargeur=1280;
@@ -133,7 +133,8 @@ int main(int argc, char** argv) //argc signifie le nombre d'argument qui sera re
             SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0, 255, 0)); //On dessine dans la surface
             SDL_BlitSurface(surface, &rectangle, temp, NULL);
             texture = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_RGBA32,SDL_TEXTUREACCESS_TARGET,nLargeur,nHauteur); //Creation de la texture
-            SDL_SetRenderTarget(renderer,texture);
+            SDL_SetRenderTarget(renderer,texture); //On definit la texture a rendre
+            SDL_RenderCopy(renderer,texture,NULL,&rTemp); //On copie la texture dans un autre rectangle
 
             SDL_Delay(3000);
 

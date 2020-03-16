cb614e2b59fdf4e3a6cba45ae3a8d67a20b0bc13
happineko <y.bonin@ludus-academie.com>
2020-03-16 13:42:25 +0100

    Question 12

--- SDL2.c
+++ SDL2.c
@@ -1,4 +1,4 @@
-//Question 10
+//Question 12
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,18 +19,28 @@ int main(int argc, char** argv) //argc signifie le nombre d'argument qui sera re
         SDL_Renderer* renderer;//Déclaration du renderer
         SDL_Point point; //On déclare un point qu'on pourra utiliser plus tard dans le programme
         SDL_Rect rectangle; //On declare un rectangle qu'on pourra utiliser plus tard dans le programme
+        int nLargeur=1280;
+        int nHauteur=720;
 
         //Pour créer la fentre on utilise la fonction SDL_CreateWindow qui attend en parametre un nom,une position x, une position y, une largeur, une hauteur et un ou plusieurs flags (disponible sur le site de SDL dans ce cas la on veut just afficher la fenêtre)
         pWindow = SDL_CreateWindow("Fenetre SDL2",SDL_WINDOWPOS_UNDEFINED,
                                                                   SDL_WINDOWPOS_UNDEFINED,
-                                                                  640,
-                                                                  480,
+                                                                  nLargeur,
+                                                                  nHauteur,
                                                                   SDL_WINDOW_SHOWN);
 
         if( pWindow ) //si la fenetre a bien été créee
         {
-            SDL_Delay(3000); //On attend trois secondes, que l'utilisateur voit la fenêtre
+            SDL_GetWindowSize(pWindow,&nLargeur,&nHauteur);
             renderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); // Création du renderer
+            rectangle.x = 0;
+            rectangle.y = 0;
+            rectangle.w = nLargeur;
+            rectangle.h = nHauteur;
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            SDL_RenderFillRect(renderer,&rectangle);
+            SDL_RenderPresent(renderer);
+            SDL_Delay(3000); //On attend trois secondes, que l'utilisateur voit la fenêtre
 
         if(renderer == NULL)//gestion des erreurs
         {

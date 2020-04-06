83d892a8cac01a96722876ccc5dae92389b56ea6
happineko <44899530+happineko@users.noreply.github.com>
2020-04-06 13:05:03 +0200

    Question 5

--- SDL2.c
+++ SDL2.c
@@ -17,7 +17,10 @@ int main(int argc, char** argv) //argc signifie le nombre d'argument qui sera re
         /* Création de la fenêtre */
         SDL_Window* pWindow = NULL; //On crée une variable fenetre de type SDL_Window
         SDL_Renderer* renderer;//Déclaration du renderer
+        SDL_Rect Sto, Gif; //Stocke la frame de l'animation
         SDL_Texture *image;
+        SDL_Event events;
+        int exitloop = 0;
         int nLargeur=1280;
         int nHauteur=720;
 
@@ -37,15 +40,45 @@ int main(int argc, char** argv) //argc signifie le nombre d'argument qui sera re
                 return EXIT_FAILURE;
             }
 
-            image = IMG_LoadTexture(renderer, "oie.png"); //on charge l'image en PNG
+            image = IMG_LoadTexture(renderer, "SpriteSheet.png"); //on charge l'image en PNG
             if (!image)
             {
                 printf("Echec de chargement de la texture : %s", SDL_GetError());
                 return -1;
             }
-            SDL_RenderCopy(renderer, image, NULL, NULL); //On applique la texture a tout l'ecran
-            SDL_RenderPresent(renderer);
-            SDL_Delay(1000);
+            Sto.x = 0;
+            Sto.y = 0;
+            Sto.w = 128;
+            Sto.h = 263;
+            Gif.x = 0;
+            Gif.y = 0;
+            Gif.w = 128;
+            Gif.h = 263;
+            while(exitloop == 0)
+            {
+                if(exitloop == 0)
+                {
+                    Sto.x = Sto.x+128;
+                    if(Sto.x > 384)//Si on dépasse la derniere frame
+                    {
+                        Sto.x = 0; //repart à la 1ere frame
+                    }
+                    SDL_RenderCopy(renderer, image, &Sto, &Gif);
+                    SDL_RenderPresent(renderer); //On Affiche le résultat ->
+                    SDL_Delay (500); //Pendant 0.001 secondes ->
+                    SDL_RenderClear(renderer); //Puis on efface le résultat
+                    SDL_Log("Appuyer sur ESPACE pour quitter");
+                }
+                while (SDL_PollEvent(&events)) {
+                    switch(events.type){
+                        case SDL_KEYDOWN: // Un événement de type touche enfoncée est effectué
+                        if (events.key.keysym.sym == SDLK_SPACE)
+                            exitloop = 1;
+                        break;
+                    }
+                }
+            }
+
             SDL_DestroyWindow(pWindow); //On "détruit" la fenetre
             SDL_DestroyRenderer(renderer);
 
